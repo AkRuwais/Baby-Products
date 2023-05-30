1144ebad315f4c14d1533b3949971e85902b4e0d
AkRuwais <akruwais9445@gmail.com>
2023-05-30 19:41:39 +0530

    more new edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import  Home  from "./Pages/Home";
+import Home from "./Pages/Home";
 import { About } from "./Pages/About";
 import { Shop } from "./Pages/Shop";
 import { Services } from "./Pages/Services";
@@ -11,32 +11,28 @@ import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Search from "./Pages/Search";
 import Cart from "./Pages/Cart";
-import  ProductCard  from "./Pages/ProductCard";
 import { createContext, useState } from "react";
- 
 
-const contest=createContext()
+const contest = createContext();
 
 function App() {
- const [cart,setcart]=useState([])
+  const [cart, setcart] = useState([]);
   return (
-    <contest.Provider value={{cart,setcart}}>
-    
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/contactus" element={<ContactUs />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/productcard" element={<ProductCard />} />
-      </Routes>
-    </BrowserRouter>
+    <contest.Provider value={{ cart, setcart }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </contest.Provider>
   );
 }
 
-export {App,contest};
+export { App, contest };
